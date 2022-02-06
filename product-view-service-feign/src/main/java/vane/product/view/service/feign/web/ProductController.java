ee7ca93fe5a14b515593f643bd1f3467c4db40b0
ClaudiaVane3 <claudia_vane_wen3@hotmail.com>
2022-02-06 23:48:36 +0800

    config-starter

--- product-view-service-feign/src/main/java/vane/product/view/service/feign/web/ProductController.java
+++ product-view-service-feign/src/main/java/vane/product/view/service/feign/web/ProductController.java
@@ -3,6 +3,8 @@ package vane.product.view.service.feign.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +13,19 @@ import vane.product.view.service.feign.pojo.Product;
 import vane.product.view.service.feign.service.ProductService;
 
 @Controller
+@RefreshScope
 public class ProductController {
 
   @Autowired ProductService productService;
+
+  @Value("${version}")
+  String version;
 
   @RequestMapping("/products")
   public Object products(Model m) {
     List<Product> ps = productService.listProducts();
     m.addAttribute("productList", ps);
+    m.addAttribute("version", version);
     return "products";
   }
 }
